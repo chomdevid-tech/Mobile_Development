25abb797ae3459193799faf81f92b63386cce54e
chomdevid-tech <chomdevid@gmail.com>
2026-06-29 14:37:21 +0700

    message

--- lib/week8/Homwork/EX-4-Handle Async fetch with a list of expenses/repository/expense_repository.dart
+++ lib/week8/Homwork/EX-4-Handle Async fetch with a list of expenses/repository/expense_repository.dart
@@ -1,12 +1,18 @@
 import '../models/expense.dart';
 
 class ExpenseRepository {
-  Expense singelExpense = Expense(amount: 45, title: "fake");
+ 
+  List<Expense> expense = [
+    Expense(title: "Flutter Course", amount: 19.99),
+    Expense(title: "Cinema", amount: 15.69),
+    Expense(title: "Salad", amount: 15.69),
+    Expense(title: "Guitar", amount: 22.69),
+  ];
 
-  Future<Expense> fetchExpense() {
+  Future<List<Expense>> fetchExpense() {
     // Simulate 5 seconds before returnning the success fetch
     return Future.delayed(Duration(seconds: 5), () {
-      return singelExpense;
+      return expense;
     });
   }
 }
