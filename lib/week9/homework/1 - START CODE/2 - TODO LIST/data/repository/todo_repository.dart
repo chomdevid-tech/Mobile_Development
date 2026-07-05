a3f8dbeea6daac8780670dbfd6cd360904547843
chomdevid <chomdevid@gmail.com>
2026-07-06 00:29:08 +0700

    Restore full week 9 todo list homework

--- lib/week9/homework/1 - START CODE/2 - TODO LIST/data/repository/todo_repository.dart
+++ lib/week9/homework/1 - START CODE/2 - TODO LIST/data/repository/todo_repository.dart
@@ -5,69 +5,63 @@ import 'package:http/http.dart' as http;
 
 import '../../models/todo.dart';
 
-class TodoRepository {
-  static final global = TodoRepository();
-  // unique instance
+// class TodoRepository {
+//   static final global = TodoRepository();   // unique instance
 
-  final String baseUrl =
-      "https://week9-ex2-default-rtdb.asia-southeast1.firebasedatabase.app";
-
-  //   final List<Todo> fakeTodos = [
-  //     Todo(id: '1', title: 'Buy groceries', completed: false),
-  //     Todo(id: '2', title: 'Finish Flutter homework', completed: true),
-  //     Todo(id: '3', title: 'Call the dentist', completed: false),
-  //     Todo(id: '4', title: 'Read 20 pages of a book', completed: true),
-  //     Todo(id: '5', title: 'Go for a 30-minute walk', completed: false),
-  //   ];
+//   final List<Todo> fakeTodos = [
+//     Todo(id: '1', title: 'Buy groceries', completed: false),
+//     Todo(id: '2', title: 'Finish Flutter homework', completed: true),
+//     Todo(id: '3', title: 'Call the dentist', completed: false),
+//     Todo(id: '4', title: 'Read 20 pages of a book', completed: true),
+//     Todo(id: '5', title: 'Go for a 30-minute walk', completed: false),
+//   ];
 
   Future<List<Todo>> getTodos() async {
+
     //  TODO
     //  Adapt the code to handle firebase data fetch
     //
 
     try {
-      Uri url = Uri.parse(
-        "$baseUrl/.json",
-      );
+      Uri url = Uri.parse("https://week9-ex2-default-rtdb.asia-southeast1.firebasedatabase.app/.json");
       final response = await http.get(url);
-      if (response.statusCode != 200) {
-        throw Exception(
-          "Failed to fetch todos. Status code: ${response.statusCode}",
-        );
+      if(response.statusCode != 200){
+        throw Exception("Failed to fetch todos. Status code: ${response.statusCode}");
       }
       final data = jsonDecode(response.body);
-      if (data == null) {
+      if(data == null){
         return [];
       }
       final Map<String, dynamic> json = Map<String, dynamic>.from(data);
       List<Todo> todos = [];
-      json.forEach((id, value) {
-        final todo = Map<String, dynamic>.from(value);
-        todos.add(
-          Todo(id: id, title: todo['title'], completed: todo['completed']),
-        );
-     
-      });
-    } catch (e) {
-      throw RepositoryException("error occurred: $e");
-    }
-  }
+      json.forEach((id, value){
+        final todojson = Map<String, dynamic>.from(value);
+        todos.add( Todo(id: id, title: json['title'], completed: json['completed'],),);
+        return gettodos;
+      
+      });}
+      catch (e){
+        throw RepositoryException("error occurred: $e");
 
-  //  TODO
-  // Ensure the message is displayed on the UI if error occured
-  //throw RepositoryException("No wifi !");
+      }
+      
+
+
+    }
+ 
+  
+
+      //  TODO
+      // Ensure the message is displayed on the UI if error occured
+      //throw RepositoryException("No wifi !");
+
+   
 
   Future<void> updateCompleted(String todoId, bool completed) async {
+    
     //  TODO
     //  Adapt the code to handle firebase data fetch
     //
-    
-
-
-
-
-
-
     int index = fakeTodos.indexWhere((e) => e.id == todoId);
     fakeTodos[index] = fakeTodos[index].copyWith(completed);
 
