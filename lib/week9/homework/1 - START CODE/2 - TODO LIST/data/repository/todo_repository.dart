7c712bc0bd7e45b413424d7c4c87c99691bbe509
chomdevid-tech <chomdevid@gmail.com>
2026-07-08 20:43:44 +0700

    message

--- lib/week9/homework/1 - START CODE/2 - TODO LIST/data/repository/todo_repository.dart
+++ lib/week9/homework/1 - START CODE/2 - TODO LIST/data/repository/todo_repository.dart
@@ -1,78 +1,76 @@
 import 'dart:convert';
 
-import 'package:all_week/week9/homework/1%20-%20START%20CODE/2%20-%20TODO%20LIST/data/repository/repository_exception.dart';
 import 'package:http/http.dart' as http;
 
 import '../../models/todo.dart';
+import '../dto/todo_dto.dart';
+import 'repository_exception.dart';
 
 class TodoRepository {
-  static final global = TodoRepository(); // unique instance
-  final String baseUrl =
-      "https://week9-ex2-default-rtdb.asia-southeast1.firebasedatabase.app";
+  static final global = TodoRepository();
 
-  //   final List<Todo> fakeTodos = [
-  //     Todo(id: '1', title: 'Buy groceries', completed: false),
-  //     Todo(id: '2', title: 'Finish Flutter homework', completed: true),
-  //     Todo(id: '3', title: 'Call the dentist', completed: false),
-  //     Todo(id: '4', title: 'Read 20 pages of a book', completed: true),
-  //     Todo(id: '5', title: 'Go for a 30-minute walk', completed: false),
-  //   ];
+  final String baseUrl =
+      'https://week9-ex2-default-rtdb.asia-southeast1.firebasedatabase.app';
 
   Future<List<Todo>> getTodos() async {
-    //  TODO
-    //  Adapt the code to handle firebase data fetch
-    //
+    Uri url = Uri.parse('$baseUrl/todos.json');
 
     try {
-      Uri url = Uri.parse("$baseUrl/.json");
-      final response = await http.get(url);
+      http.Response response = await http.get(url);
+
+     
+
       if (response.statusCode != 200) {
-        throw Exception(
-          "Failed to fetch todos. Status code: ${response.statusCode}",
-        );
+        throw RepositoryException('Failed to fetch todos');
       }
+
       final data = jsonDecode(response.body);
+
       if (data == null) {
         return [];
       }
-      final Map<String, dynamic> json = Map<String, dynamic>.from(data);
+
       List<Todo> todos = [];
-      json.forEach((id, value) {
-        final todo = Map<String, dynamic>.from(value);
-        todos.add(
-          Todo(id: id, title: todo['title'], completed: todo['completed']),
-        );
-      });
-      return todos;
+
+      if (data is List) {
+        for (int i = 0; i < data.length; i++) {
+          final item = data[i];
+
+          if (item == null) {
+            continue;
+          }
+
+          Todo todo = TodoDto.fromJson(
+            i.toString(),
+            Map<String, dynamic>.from(item),
+          );
+
+          todos.add(todo);
+        }
+
+        return todos;
+      }
+
+      throw RepositoryException('Invalid Firebase data format');
     } catch (e) {
-      throw RepositoryException("error occurred: $e");
+      throw RepositoryException('Unexpected error');
     }
   }
 
-  //  TODO
-  // Ensure the message is displayed on the UI if error occured
-  //throw RepositoryException("No wifi !");
-
   Future<void> updateCompleted(String todoId, bool completed) async {
-    //  TODO
-    //  Adapt the code to handle firebase data fetch
-    //
+    Uri url = Uri.parse('$baseUrl/todos/$todoId.json');
+
     try {
-      Uri url = Uri.parse("$baseUrl/todos/$todoId.json");
-      final response = await http.patch(
+      http.Response response = await http.patch(
         url,
-        body: jsonEncode({"completed": completed}),
+        body: jsonEncode({'completed': completed}),
       );
+
       if (response.statusCode != 200) {
-        throw Exception("fail to update");
+        throw RepositoryException('Failed to update todo');
       }
     } catch (e) {
-      throw RepositoryException("error occrured: $e");
+      throw RepositoryException('Unexpected error');
     }
-
-  //   int index = fakeTodos.indexWhere((e) => e.id == todoId);
-  //   fakeTodos[index] = fakeTodos[index].copyWith(completed);
-
-  //   return Future.delayed(Duration(microseconds: 1), () => true);
-   }
+  }
 }
